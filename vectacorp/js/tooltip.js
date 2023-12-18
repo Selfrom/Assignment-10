07e64a1fb236862acc6c89f73b3159f77ad50cc4
Selfrom <kanghyuklee1@gmail.com>
2023-12-17 17:39:06 -0800

    Finished

---
+++ vectacorp/js/tooltip.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("a[href*='index']").attr("title", "Get the latest from VectaCorp").tooltip();
+    $("a[href*='aboutus']").attr("title", "Meet our award winning team").tooltip();
+    $("a[href*='solutions']").attr("title", "Our solutions for you!").tooltip();
+    $("a[href*='support']").attr("title", "How can we help you?").tooltip();
+    $("a[href*='contactus']").attr("title", "Our contact info").tooltip();
+});
